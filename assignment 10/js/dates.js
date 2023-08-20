195469762bc3333f73d877d340fb8cfd6fa4876c
Ruby Verma <rubyverma@Rubys-MacBook-Air.local>
2023-08-19 19:47:11 -0700

    update in date.js
    some improvment in step2

--- assignment 10/js/dates.js
+++ assignment 10/js/dates.js
@@ -1,24 +1,28 @@
 //STEP 1
 let currentMonth = new Date().getMonth();
-
 function myFun(myMonth){
     const myDays = [31,28,30,31,30,31,30,31,30,31,30,31];
  return myDays[myMonth];
 }
-
-
 let result = myFun(currentMonth);
 console.log(`There are ${result} days in this month`);
 
 
 //STEP 2
-// let m1=d.getMonth();
-// console.log(m1)
+let myDay=new Date('2023-08-19').getMonth();
+function myMonth(day){
+    const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+    return monthNames[day];
+}
+let monthResult=myMonth(myDay);
+console.log(`Month is ${monthResult}`)
 
 //STEP 3
 let d3=new Date();
-    console.log(d3)
-    //console.log(d.getDate();
+    
 if(d3.getDay()=== 6||d3.getDay() === 0)
 {
     console.log("This is Weekend");
@@ -34,9 +38,7 @@ let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 let currentDate = new Date();
 let currentDayIndex = currentDate.getDay(); // Get today's day index (0-6)
 let yesterdayDayIndex = (currentDayIndex - 1 + 7) % 7; // Calculate yesterday's day index
-
 let yesterdayDay = daysOfWeek[yesterdayDayIndex];
-
 console.log("Yesterday's day of the week was:", yesterdayDay);
 
 
